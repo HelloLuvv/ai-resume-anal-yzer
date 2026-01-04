032b19e5368b054df1aa5bf033a070f98ccb5663
Luv <luvambadiya@gmail.com>
2026-01-04 23:55:02 +0530

    Fix imports with webpack alias in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
   output: 'export',
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
